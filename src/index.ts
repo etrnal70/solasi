fb2fe8e70df27ecb145f16c100548ac48199eeb1
Mochammad Hanif R <etrnal70@gmail.com>
2022-12-18 22:08:50 +0700

    setup swagger ui
    cleanup later

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,29 @@ import { replyRoute } from "./core/reply/reply.route";
 import { topicRoute } from "./core/topic/topic.route";
 import { userRoute } from "./core/user/user.route";
 import { uploadRoute } from "./utils/upload/upload.route";
+import swaggerUiExpress from "swagger-ui-express";
+import * as swaggerDocs from "./swagger-output.json";
 
 dotenv.config();
 
 const PORT: number = parseInt(process.env.PORT as string, 10);
 const server: Express = express();
 
+// TODO Move to somewhat dev command ?
+// Documentation
+// const docFile = "./swagger-output.json";
+// const routes = [
+//     path.join(__dirname, "core/user/user.route.ts"),
+//     path.join(__dirname, "core/topic/topic.route.ts"),
+//     path.join(__dirname, "core/reply/reply.route.ts"),
+//     path.join(__dirname, "core/question/question.route.ts"),
+// ];
+// swaggerAutogen()(docFile, routes);
+
 server.use(express.json());
 server.use(express.static("public"));
+server.use("/docs", swaggerUiExpress.serve);
+server.get("/docs", swaggerUiExpress.setup(swaggerDocs));
 
 server.use(uploadRoute);
 server.use(userRoute);

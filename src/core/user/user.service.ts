3a43b4d9c2db2233ad036ca64f604e94b4e81fd0
Mochammad Hanif R <etrnal70@gmail.com>
2023-01-04 23:17:10 +0700

    fix(user): improve

--- src/core/user/user.service.ts
+++ src/core/user/user.service.ts
@@ -5,133 +5,135 @@ import { compareSync, hashSync } from "bcrypt";
 import { generateToken } from "../../utils/auth/auth.service";
 
 export async function registerUser(data: UserCore) {
-  const generatedUsername = generateFromEmail(data.email);
-  const hashedPassword = hashSync(data.password, 10);
-  const user = await dbConn.user.create({
-    data: {
-      email: data.email,
-      password: hashedPassword,
-      username: generatedUsername,
-    },
-  });
+    const generatedUsername = generateFromEmail(data.email);
+    const hashedPassword = hashSync(data.password, 10);
+    const user = await dbConn.user.create({
+        data: {
+            email: data.email,
+            password: hashedPassword,
+            username: generatedUsername,
+        },
+    });
 
-  return user;
+    return user;
 }
 
 export async function verifyCredsUser(data: UserCore) {
-  const user = await dbConn.user.findFirst({
-    where: {
-      email: data.email,
-    },
-    select: {
-      id: true,
-      email: true,
-      username: true,
-      profileImage: true,
-      description: true,
-      createdAt: true,
-      updatedAt: true,
-      password: true,
-    },
-  });
+    const user = await dbConn.user.findFirst({
+        where: {
+            email: data.email,
+        },
+        select: {
+            id: true,
+            email: true,
+            username: true,
+            profileImage: true,
+            description: true,
+            createdAt: true,
+            updatedAt: true,
+            password: true,
+        },
+    });
 
-  if (user) {
-    if (compareSync(data.password, user.password)) {
-      try {
-        const userId = user.id;
-        const tokenPair = await generateToken(userId);
+    if (user) {
+        if (compareSync(data.password, user.password)) {
+            try {
+                const userId = user.id;
+                const tokenPair = await generateToken(userId);
 
-        return {
-          token: tokenPair,
-          ...user,
-        };
-      } catch (e) {
-        return e;
-      }
-    } else return Error("Password mismatch");
-  } else {
-    return Error("User not found");
-  }
+                return {
+                    token: tokenPair,
+                    user,
+                };
+            } catch (e) {
+                return e;
+            }
+        } else return Error("Password mismatch");
+    } else {
+        return Error("User not found");
+    }
 }
 
 export async function deleteCredsUser(userId: number) {
-  return await dbConn.userCredential.deleteMany({
-    where: {
-      userId,
-    },
-  });
+    return await dbConn.userCredential.deleteMany({
+        where: {
+            userId,
+        },
+    });
 }
 
 export async function refreshCredsUser(userId: number) {
-  await deleteCredsUser(userId);
-  return await generateToken(userId);
+    await deleteCredsUser(userId);
+    return await generateToken(userId);
 }
 
 export async function findUser(keyword: string) {
-  const users = await dbConn.user.findMany({
-    where: {
-      username: {
-        search: keyword,
-      },
-    },
-    select: {
-      id: true,
-      username: true,
-      profileImage: true,
-      description: true,
-      createdAt: true,
-    },
-  });
+    const users = await dbConn.user.findMany({
+        where: {
+            username: {
+                search: keyword,
+            },
+        },
+        select: {
+            id: true,
+            username: true,
+            profileImage: true,
+            description: true,
+            createdAt: true,
+        },
+    });
 
-  return users;
+    return users;
 }
 
 export async function getProfileUser(userId: number) {
-  const user = await dbConn.user.findFirst({
-    where: { id: userId },
-    select: {
-      _count: { select: { acceptedUpvotes: true, acceptedDownvotes: true } },
-      questions: {
+    const user = await dbConn.user.findFirst({
+        where: { id: userId },
         select: {
-          id: true,
-          topic: { select: { id: true, name: true, profileImage: true } },
-          body: true,
-          image: true,
-          createdAt: true,
-          updatedAt: true,
+            _count: { select: { acceptedUpvotes: true, acceptedDownvotes: true } },
+            questions: {
+                select: {
+                    id: true,
+                    topic: { select: { id: true, name: true, profileImage: true, grade: true } },
+                    body: true,
+                    choices: { select: { body: true, createdAt: true, updatedAt: true } },
+                    image: true,
+                    type: true,
+                    createdAt: true,
+                    updatedAt: true,
+                },
+            },
+            id: true,
+            username: true,
+            email: true,
+            profileImage: true,
+            description: true,
+            createdAt: true,
+            updatedAt: true,
+            password: false,
         },
-      },
-      id: true,
-      username: true,
-      email: true,
-      profileImage: true,
-      description: true,
-      createdAt: true,
-      updatedAt: true,
-      password: false,
-    },
-  });
+    });
 
-  return user;
+    return user;
 }
 
 export async function updateProfileUser(data: UserProfile, userId: number) {
-  const updatedUser = await dbConn.user.update({
-    where: {
-      id: userId,
-    },
-    data: data,
-    select: {
-      id: true,
-      username: true,
-      email: true,
-      profileImage: true,
-      description: true,
-      createdAt: true,
-      updatedAt: true,
-      password: false,
-    },
-  });
+    const updatedUser = await dbConn.user.update({
+        where: {
+            id: userId,
+        },
+        data: data,
+        select: {
+            id: true,
+            username: true,
+            email: true,
+            profileImage: true,
+            description: true,
+            createdAt: true,
+            updatedAt: true,
+            password: false,
+        },
+    });
 
-  return updatedUser;
+    return updatedUser;
 }

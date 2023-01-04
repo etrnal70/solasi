a9799341645e0c53eb9307e07e29e0c14fae9fb6
Mochammad Hanif R <etrnal70@gmail.com>
2023-01-05 01:07:42 +0700

    fix(upload): handle error case, fix link format

--- src/utils/upload/upload.controller.ts
+++ src/utils/upload/upload.controller.ts
@@ -3,13 +3,10 @@ import { Request, Response } from "express";
 export async function upload(req: Request, res: Response) {
   const file = req.file;
   if (file !== undefined) {
-    const DOMAIN = process.env.DOMAIN as string;
-    const PORT = process.env.PORT as string;
-    const HOST = process.env.HOST as string;
-    if (DOMAIN === "") {
-      return res.status(201).json({ url: `http://${HOST}:${PORT}/${file.filename}` });
+    if (process.env.DOMAIN == "" || process.env.DOMAIN === undefined) {
+      return res.status(201).json({ url: `http://${process.env.HOST}:${process.env.PORT}/static/${file.filename}` });
     } else {
-      return res.status(201).json({ url: `http://${DOMAIN}/${file.filename}` });
+      return res.status(201).json({ url: `http://${process.env.DOMAIN}/static/${file.filename}` });
     }
   }
 

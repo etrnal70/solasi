4f7380a1b4d4ea040ca061c14823b3a85c6f00e3
Mochammad Hanif R <etrnal70@gmail.com>
2023-01-06 09:38:41 +0700

    refactor(search): use zod enums for validation instead

--- src/core/search/search.controller.ts
+++ src/core/search/search.controller.ts
@@ -7,22 +7,21 @@ import { findUser } from "../user/user.service";
 export async function search(req: Request, res: Response) {
   try {
     const { scope, keyword, grade, type, topicId } = req.query;
-    if (scope === "user") {
+    if (scope == "user") {
       const users = await findUser(keyword as string);
 
       return res.status(200).json(users);
-    } else if (scope === "topic") {
+    } else if (scope == "topic") {
       const topics = await findTopic(grade as string as TopicGrade, keyword as string);
       return res.status(200).json(topics);
-    } else if (scope === "question") {
+    } else if (scope == "question") {
       if ((topicId as string) == "") {
         const questions = await findQuestion(keyword as string, type as string as QuestionType);
         return res.status(200).json(questions);
+      } else {
+        const questions = await findQuestion(keyword as string, type as string as QuestionType);
+        return res.status(200).json(questions);
       }
-    } else if (scope === "") {
-      return res.status(400).json({ message: "Missing required scope query param" });
-    } else {
-      return res.status(400).json({ message: "Unknown scope" });
     }
   } catch (e) {
     if (e instanceof Error) return res.status(500).json({ message: e.message });
--- src/core/search/search.schema.ts
+++ src/core/search/search.schema.ts
@@ -1,9 +1,10 @@
 import { QuestionType, TopicGrade } from "@prisma/client";
 import { z } from "zod";
 
+const scopeVals = ["user", "topic", "question"] as const;
 export const searchSchema = z.object({
   query: z.object({
-    scope: z.string(),
+    scope: z.enum(scopeVals),
     keyword: z.string(),
     grade: z.nativeEnum(TopicGrade).optional(),
     type: z.nativeEnum(QuestionType).optional(),

32d5e0f561906135f9d4a4a0ba62ce8ed01cf3bf
Mochammad Hanif R <etrnal70@gmail.com>
2023-01-06 09:38:18 +0700

    fix(upload): improve error handling

--- src/utils/upload/upload.route.ts
+++ src/utils/upload/upload.route.ts
@@ -1,8 +1,19 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import multer from "multer";
 import { upload } from "./upload.controller";
 
-const uploadHandler = multer({ dest: "public/static/" });
+const uploadHandler = multer({ dest: "public/static/" }).single("image");
 export const uploadRoute: Router = Router();
 
-uploadRoute.post("/upload", uploadHandler.single("image"), upload);
+uploadRoute.post(
+  "/upload",
+  function (req: Request, res: Response, next: NextFunction) {
+    uploadHandler(req, res, function (err) {
+      if (err instanceof Error) {
+        return res.status(500).json({ message: `error: failed to upload file: ${err.message}` });
+      }
+      next();
+    });
+  },
+  upload,
+);

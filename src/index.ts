b5bdd9fffba0219d908fcf89aff72f24a7398bbb
Mochammad Hanif R <etrnal70@gmail.com>
2023-01-10 20:49:18 +0700

    feat(server): implement ssl import

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express, { Express } from "express";
+import { readFileSync } from "fs";
+import * as https from "https";
 import { leaderboardRoute } from "./core/leaderboard/leaderboard.route";
 import { questionRoute } from "./core/question/question.route";
 import { replyRoute } from "./core/reply/reply.route";
@@ -26,9 +28,17 @@ server.use(replyRoute);
 server.use(searchRoute);
 server.use(leaderboardRoute);
 
+const secureServer = https.createServer(
+  {
+    key: readFileSync(process.env.KEY_LOCATION as string),
+    cert: readFileSync(process.env.CERT_LOCATION as string),
+  },
+  server,
+);
+
 async function start(): Promise<void> {
   try {
-    server.listen(PORT, () => {
+    secureServer.listen(PORT, () => {
       console.log(`Listening on port ${PORT}`);
     });
   } catch (err) {
